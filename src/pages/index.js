2e030c2670c7fe8e0b9e5b88f75213ae0dce491d
Kaizmaker <kaizmaker@gmail.com>
2019-05-07 22:27:09 +0800

    @@

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ const IndexPage = () => (
     <h1>Hi  I'm Kaiz</h1>
     <p>Welcome to your new Gatsby site.SSSSS</p>
     <p>Now go build something great.</p>
-    <Link to="/page-222333/">到第二頁</Link>
+    <Link to="/page-222333/">到第二頁000</Link>
   </div>
 )
 
