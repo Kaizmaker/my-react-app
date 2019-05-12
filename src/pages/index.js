df1c6afdef11b5c59545c5ae959df09d71068004
Kaizmaker <kaizmaker@gmail.com>
2019-05-12 21:54:59 +0800

    改版

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,15 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
+    <div className="Hero">
+    <div className="HeroGroup">
     <h1>Hi  I'm Kaiz</h1>
     <p>Welcome to your new Gatsby site.SSSSS</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">到第二頁0</Link>
+    </div>
+    </div>
+    
   </div>
 )
 
